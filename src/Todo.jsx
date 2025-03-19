1bfb210e2ebea516871e25f76aa24bc2caad59f9
Nathan Davidov <NathanDavidov9@gmail.com>
2025-03-19 01:34:38 -0400

    initial

---
+++ src/Todo.jsx
@@ -0,0 +1,33 @@
+import { ACTIONS } from "./App";
+function Todo({ todo, dispatch }) {
+    return (
+        <>
+            <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+                {todo.name}
+            </span>
+            <button
+                onClick={() => {
+                    dispatch({
+                        type: ACTIONS.TOGGLE_TODO,
+                        payload: { id: todo.id },
+                    });
+                }}
+            >
+                Toggle
+            </button>
+            <button
+                onClick={() => {
+                    dispatch({
+                        type: ACTIONS.DELETE_TODO,
+                        payload: { id: todo.id },
+                    });
+                }}
+            >
+                Delete
+            </button>
+            <input type="checkbox"></input>
+        </>
+    );
+}
+
+export default Todo;
